f6f1bf6358b140582f8fe5c4ba1bb7da6add1eab
aaron.feigenbaum <aaron@enervee.com>
2020-03-01 23:24:28 -0800

    Dockerized project, start on server

--- src/components/TotalBudget.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Budget } from '../types';
-
-export const TotalBudget = ({ budgetAmount }: Budget) => (
-    <>
-        <p className="has-text-weight-bold">YOUR BALANCE</p>
-        <p className="has-text-info is-size-4">${budgetAmount.toFixed(2)}</p>
-    </>
-)
