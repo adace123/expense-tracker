75de911a633127bd4c67328e21c7cf526f5c8a4d
aaron.feigenbaum <aaron@enervee.com>
2020-03-05 23:54:49 -0800

    server working

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -3,7 +3,7 @@ export default {
         transactions: () => [
             {type: 'CREDIT', description: 'refund', merchant: 'ABC Co.', category: 'GROCERIES', amount: 123, _id: 456, date: new Date().toISOString()}
         ],
-        transaction: (id) => ({
+        transaction: (id: Number) => ({
             type: 'DEBIT', description: 'lunch', merchant: 'Starbucks', category: 'RESTAURANT', amount: 10, _id: 101, date: new Date().toISOString()
         })
     }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 import { ApolloServer } from 'apollo-server';
 
 import typeDefs from './schema';
@@ -11,11 +11,10 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen(8080).then(({ url }) => {
     console.log(`Server started at ${url}`);
-    mongoose.connect(process.env.MONGO_CONNECTION_STRING, { useNewUrlParser: true }, err => {
+    
+    mongoose.connect(process.env.MONGO_CONNECTION_STRING || '', { useNewUrlParser: true }, err => {
         if (err)
-            console.log(err);
+            throw err;
         else console.log('connected');
     });
 });
-
-
