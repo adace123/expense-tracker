c6e90fa4c0a1f27a0b9f5cb037edf3fb89c00407
aaron.feigenbaum <aaron@enervee.com>
2020-03-07 21:33:41 -0800

    created test GraphQL resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,10 +1,7 @@
+import transactionModel from './models/transactionModel';
+
 export default {
     Query: {
-        transactions: () => [
-            {type: 'CREDIT', description: 'refund', merchant: 'ABC Co.', category: 'GROCERIES', amount: 123, _id: 456, date: new Date().toISOString()}
-        ],
-        transaction: (id: Number) => ({
-            type: 'DEBIT', description: 'lunch', merchant: 'Starbucks', category: 'RESTAURANT', amount: 10, _id: 101, date: new Date().toISOString()
-        })
+        transactions: () => transactionModel.find()
     }
 }
