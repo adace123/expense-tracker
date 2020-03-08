b2503c97975a266b3551469d5fa4018aeceb92f0
aaron.feigenbaum <aaron@enervee.com>
2020-03-07 22:21:40 -0800

    added single transaction resolver

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,7 +1,9 @@
 import transactionModel from './models/transactionModel';
+import * as ApolloServer from 'apollo-server';
 
 export default {
     Query: {
-        transactions: () => transactionModel.find()
+        transactions: () => transactionModel.find(),
+        transaction: (parent, { id }, context, info) => transactionModel.findById(id)
     }
 }
