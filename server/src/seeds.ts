8c576da9df13479758f65f11c755b5dac6f105b6
Aaron Feigenbaum <rexforddrive@gmail.com>
2020-03-08 18:12:58 -0700

    create fake users in seeds file

--- server/src/seeds.ts
+++ server/src/seeds.ts
@@ -1,47 +1,82 @@
 import { Command } from 'commander'; 
 import * as faker from 'faker';
 import * as _ from 'lodash';
+
 import * as mongoose from 'mongoose';
 
+import User from '../../types/user';
 import { Transaction, Category, TransactionType } from '../../types/transaction';
 import transactionModel from './models/transactionModel';
+import userModel from './models/userModel';
 import dbConnect from './db';
 
 const command = new Command();
 
-export function generateFakeTransactions(amount: number): Array<Transaction> {
-    return _.range(0, amount).map(i => {
-        const randomCat: Category = _.sample(Object.keys(Category)) as Category;
+interface FakeData {
+    users: Array<User>;
+    transactions: Array<Transaction>;
+}
+
+function preprocessBulkWrite(documentArray: Array<User | Transaction>): any[] {
+    return documentArray.map(document => ({ insertOne: { document } }));
+}
+
+export function generateFakeData(transactionCount: number, usercount: number): FakeData {
+    const fakeUsers: Array<User> = _.range(0, usercount).map(i => (
+        {
+            _id: new mongoose.Types.ObjectId(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            transactions: []
+        }
+    ));
+
+    const fakeTransactions = _.range(0, transactionCount).map(i => {
+        const randomCategory: Category = _.sample(Object.keys(Category)) as Category;
         const randomTransactionType: TransactionType = _.sample(Object.keys(TransactionType)) as TransactionType
-        
-        const document = {
+        const user: User = _.sample(fakeUsers) as User;
+
+        const transaction = {
+            _id: new mongoose.Types.ObjectId(),
             date: faker.date.past(),
-            category: randomCat,
+            category: randomCategory,
             type: randomTransactionType,
             description: faker.lorem.sentence(),
             amount: parseFloat(faker.commerce.price()),
-            merchant: faker.company.companyName()
+            merchant: faker.company.companyName(),
+            user: user._id
         }
-        return document;
+        
+        user.transactions.push(transaction);
+        return transaction;
     });
+
+    return {
+        users: fakeUsers,
+        transactions: fakeTransactions
+    };
 }
 
 command
-    .option('-n, --numRecords <number>', 'Number of records to save', 10)
+    .option('-t, --numTransactions <number>', 'Number of transactions to save', 10)
+    .option('-u, --numUsers <number>', 'Number of users to save', 5)
     .option('--no-reset', 'Delete transaction-test collection before saving')
     .parse(process.argv);
 
 dbConnect().then(async connection => {
-    const fakeTransactionRecords = generateFakeTransactions(command.numRecords)
-        .map(document => ({insertOne: { document }}));
+    const { users, transactions } = generateFakeData(command.numTransactions, command.numUsers);
     
     if (command.reset) {
-        transactionModel.collection.drop();
+        userModel.countDocuments((_, count) => {
+            count && userModel.collection.drop();
+        });
     }
 
     try {
-        await transactionModel.bulkWrite(fakeTransactionRecords);
-        console.log(`Wrote ${command.numRecords} fake transactions to the DB.`);
+        await transactionModel.bulkWrite(preprocessBulkWrite(transactions));
+        await userModel.bulkWrite(preprocessBulkWrite(users));
+        console.log(`Wrote ${command.numUsers} fake users to the DB.`);
+        console.log(`Wrote ${command.numTransactions} fake transactions to the DB.`);
     } catch(e) {
         console.log(e);
     } finally {
