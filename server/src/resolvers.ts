cc1e14e4b3205262dc1b81f04269263df3c3451e
Aaron Feigenbaum <rexforddrive@gmail.com>
2020-03-08 18:16:23 -0700

    set default date for newly created transactions, add users + user resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,22 +1,27 @@
 import * as mongoose from 'mongoose';
 
 import transactionModel from './models/transactionModel';
-import { Transaction } from '../../types/transaction';
+import userModel from './models/userModel';
 
 export default {
     Query: {
         transactions(): Promise<mongoose.Document[]> {
-            return transactionModel.find().exec();
+            return transactionModel.find().populate('user').exec();
         },
         transaction(parent, { id }, context, info): Promise<mongoose.Document | null> {
             return transactionModel.findById(id).exec();
+        },
+        users(): Promise<mongoose.Document[]> {
+            return userModel.find().populate('transactions').exec();
+        },
+        user(parent, { id }, context, info): Promise<mongoose.Document | null> {
+            return userModel.findById(id).populate('transactions').exec();
         }
     },
     Mutation: {
         createTransaction(parent, { transaction }, context, info): Promise<mongoose.Document> {
-            transaction.date = new Date();
-            const newTransaction = new transactionModel(transaction);
-            return newTransaction.save();
+            transaction.date = transaction.date || new Date();
+            return transactionModel.create(transaction);
         },
         deleteTransaction(parent, { id }, context, info): Promise<mongoose.Document | null> {
             return transactionModel.findByIdAndDelete(id).exec();
