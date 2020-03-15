22362b085307db21ebf3e68469e07b3c54eaa699
aaron.feigenbaum <aaron@enervee.com>
2020-03-14 19:56:42 -0700

    update path in transactionModel

--- server/src/models/transactionModel.ts
+++ server/src/models/transactionModel.ts
@@ -1,5 +1,5 @@
 import * as mongoose from 'mongoose';
-import { Category, TransactionType } from '../../../types/transaction';
+import { Category, TransactionType } from '../types/transaction';
 
 const TransactionSchema = new mongoose.Schema({
     category: {
