b6f65f975bbe4712ad1dabd7f4e5d0d252faf821
aaron.feigenbaum <aaron@enervee.com>
2020-03-14 19:55:45 -0700

    use Docker path for .env, be able to connect to prod or dev DBs

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,14 +1,19 @@
 import * as mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({
+    path: '/app/src/.env'
+});
 
 export default async (): Promise<mongoose.ConnectionBase> => {
+    const nodeEnv: string = process.env.NODE_ENV || 'DEV';
+    const connectionString = process.env[`MONGO_${nodeEnv}_DB_CONNECTION_STRING`];
+
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_CONNECTION_STRING as string, { 
+        const { connection } = await mongoose.connect(connectionString as string, { 
             useNewUrlParser: true, useUnifiedTopology: true, keepAlive: true, connectTimeoutMS: 30000
          })
-        console.log('Mongo connection established');
+        console.log(`Successfully connected to the ${nodeEnv} database.`);
         return connection;
     } catch(e) {
         throw e;
